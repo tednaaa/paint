7eb1f117c8852d56038e07c917979c7a5612ce43
zhebil <jbilyk1996@gmail.com>
2021-05-22 18:06:53 +0300

    fix

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-console.log(path.resolve(__dirname, 'client', 'build'));
+console.log(path.resolve(__dirname, '../../', 'client', 'build'));
 
 app.use('/', router);
 
