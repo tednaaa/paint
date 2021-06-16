74cabcc472209ea9cf19395210346f08715fb065
And <13and.bulk@gmail.com>
2021-06-16 14:38:01 +0400

    don't forget :// in url

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,6 +4,6 @@ export const DEFAULT_DRAW_COLOR = '#000';
 
 export const { REACT_APP_API_URL } = process.env;
 
-export const SOCKET_URL = isProductionEnv()
-  ? `wss://${REACT_APP_API_URL?.replace('https//', '')}`
+export const SOCKET_URL = !isProductionEnv()
+  ? `wss://${REACT_APP_API_URL?.replace('https://', '')}`
   : `ws://${REACT_APP_API_URL?.split('://')[1]}`;
