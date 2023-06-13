5123f7252c94959a0ddfc1fc076ff7ba66f7598f
Andranik <79831859+tednaaa@users.noreply.github.com>
2023-06-13 20:14:07 +0400

    Delete App.tsx

--- src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import { RouterProvider } from 'react-router-dom';
-
-import { router } from '@/pages/router';
-
-export const App: FC = () => {
-  return <RouterProvider router={router} />;
-};
